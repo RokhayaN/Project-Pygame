ff1e79f3e24d45e5c9aab180aac62e7004939288
RokhayaN <55556810+RokhayaN@users.noreply.github.com>
2022-11-24 21:53:59 -0600

    Update Pygame.py
    created instances of class

--- Pygame.py
+++ Pygame.py
@@ -1,5 +1,11 @@
+#creating our class
 class Character:
     def __init__(self, name, health):
         self.name = name
         self.health = health
 
+
+#creating instances of our class 
+
+Hulk = Character('hulk', 10)
+Batman = Character("batman", 8)
