61a56128903c81a7f3580ac615c44e1ad45268ee
RokhayaN <55556810+RokhayaN@users.noreply.github.com>
2022-11-28 12:07:02 -0600

    Update Pygame.py
    Adding subclasses to our Character class

--- Pygame.py
+++ Pygame.py
@@ -4,10 +4,25 @@ class Character:
         self.name = name
         self.health = health
 
+#Inheriting our parent class - Subclasses Hero and Vilain
+class Hero(Character):
+    def __init__(self, name, health): 
+        super().__init__(name,health)
+
+
+class Vilain(Character):
+    def __init__(self, name, health): 
+        super().__init__(name,health)
+
+#create our Heroes + Vilains List
+
+Heroes = ["Hulk", "Batman", "Spiderman"]
+Vilains = ["Goblin","Zombie","Monster"]
 
 #creating instances of our class 
 
-Hulk = Character("hulk", 10)
-Batman = Character("batman", 8)
+Hulk = Hero("hulk", 10)
+Batman = Hero("batman", 8)
+Superman = Hero("Superman", 9)
 
 
